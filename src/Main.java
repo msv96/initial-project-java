2b2311011e6188606ff5c69261838bbe2b2883a4
Srinivasan M <msrini1100@gmail.com>
2023-07-16 11:54:50 +0530

    tried scanner

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         int variable = 8;
         char character = 'q';
         String str = "Hello and welcome!";
         System.out.println("i = " + variable + str + character);
+
+        System.out.println(56 % 5);
+
+        Scanner sc = new Scanner(System.in);
+
+        String scanned = sc.next();
+
+        System.out.println(scanned);
+
+        int scanned1 = sc.nextInt();
+
+        System.out.println(scanned1);
     }
 }
