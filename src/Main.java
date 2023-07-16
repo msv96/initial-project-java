338f4e9bf2533748489dd3ebba2f4d6d6a26569e
Srinivasan M <msrini1100@gmail.com>
2023-07-16 11:09:12 +0530

    learned primitive data types

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        int variable = 8;
+        char character = 'q';
+        String str = "Hello and welcome!";
+        System.out.println("i = " + variable + str + character);
     }
 }
